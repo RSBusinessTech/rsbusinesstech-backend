e30cfd38f24ade8cc6f236dcc09aef4231245e2e
RS Business Tech <rsbusinesstech@gmail.com>
2025-08-31 23:15:44 +0530

    Commenting Db related code

--- src/main/java/com/rsbusinesstech/rsbusinesstech_backend/contact/repository/CompanyRepository.java
+++ src/main/java/com/rsbusinesstech/rsbusinesstech_backend/contact/repository/CompanyRepository.java
@@ -1,9 +1,9 @@
-package com.rsbusinesstech.rsbusinesstech_backend.contact.repository;
-
-import com.rsbusinesstech.rsbusinesstech_backend.contact.model.Company;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CompanyRepository extends JpaRepository<Company,Integer> {
-}
+//package com.rsbusinesstech.rsbusinesstech_backend.contact.repository;
+//
+//import com.rsbusinesstech.rsbusinesstech_backend.contact.model.Company;
+//import org.springframework.data.jpa.repository.JpaRepository;
+//import org.springframework.stereotype.Repository;
+//
+//@Repository
+//public interface CompanyRepository extends JpaRepository<Company,Integer> {
+//}
--- src/main/java/com/rsbusinesstech/rsbusinesstech_backend/contact/service/CompanyService.java
+++ src/main/java/com/rsbusinesstech/rsbusinesstech_backend/contact/service/CompanyService.java
@@ -1,29 +1,29 @@
-package com.rsbusinesstech.rsbusinesstech_backend.contact.service;
-
-import com.rsbusinesstech.rsbusinesstech_backend.contact.model.Company;
-import com.rsbusinesstech.rsbusinesstech_backend.contact.repository.CompanyRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
-import java.util.List;
-import java.util.Optional;
-
-@Service
-public class CompanyService {
-
-    @Autowired
-    CompanyRepository companyRepository;
-
-    public String saveCompany(Company company){
-        companyRepository.save(company);
-        return "Company details saved successfully...";
-    }
-
-    public String getCompanyEmail(){
-        Optional<Company> optionalCompany = companyRepository.findById(1);
-        if(optionalCompany.isPresent()){
-            return optionalCompany.get().getEmail();
-        }
-        return "Failed to fetch email";
-    }
-}
+//package com.rsbusinesstech.rsbusinesstech_backend.contact.service;
+//
+//import com.rsbusinesstech.rsbusinesstech_backend.contact.model.Company;
+//import com.rsbusinesstech.rsbusinesstech_backend.contact.repository.CompanyRepository;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.stereotype.Service;
+//
+//import java.util.List;
+//import java.util.Optional;
+//
+//@Service
+//public class CompanyService {
+//
+//    @Autowired
+//    CompanyRepository companyRepository;
+//
+//    public String saveCompany(Company company){
+//        companyRepository.save(company);
+//        return "Company details saved successfully...";
+//    }
+//
+//    public String getCompanyEmail(){
+//        Optional<Company> optionalCompany = companyRepository.findById(1);
+//        if(optionalCompany.isPresent()){
+//            return optionalCompany.get().getEmail();
+//        }
+//        return "Failed to fetch email";
+//    }
+//}
