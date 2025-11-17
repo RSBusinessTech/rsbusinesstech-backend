0202e3226dc28fcb9063946cf8f1ddfb6e1a1051
RS Business Tech <rsbusinesstech@gmail.com>
2025-11-17 20:41:54 +0530

    Modifying properties

--- src/main/java/com/rsbusinesstech/rsbusinesstech_backend/property/service/PropertyService.java
+++ src/main/java/com/rsbusinesstech/rsbusinesstech_backend/property/service/PropertyService.java
@@ -13,42 +13,42 @@ public class PropertyService
     public List<PropertyDTO> getPropertiesByType(String type){
         List<PropertyDTO> properties = new ArrayList<>();
 
+        List<String> amenities = Arrays.asList(
+                "Air conditioner",
+                "Balcony",
+                "Bed",
+                "Covered car parking",
+                "Refrigerator",
+                "Washing machine",
+                "Water heater",
+                "Wardrobe",
+                "Sofa set",
+                "Dining table",
+                "Microwave oven",
+                "TV unit",
+                "High-speed WiFi",
+                "24-hour security"
+        );
+
+        List<String> commonFacilities = new ArrayList<>(Arrays.asList(
+                "Gym",
+                "Balcony",
+                "Covered car parking",
+                "TV unit",
+                "High-speed WiFi",
+                "24-hour security"
+        ));
+
+        List<String> imageUrls = new ArrayList<>(Arrays.asList(
+                "assets/images/commercial-hero.webp",
+                "assets/images/kl.webp",
+                "assets/images/kl-rent-bg.webp",
+                "assets/images/commercial-hero.webp",
+                "assets/images/buy-hero.webp",
+                "assets/images/kl-rent-bg.webp"
+        ));
+
         if(type.equalsIgnoreCase("rent")){
-            List<String> amenities = Arrays.asList(
-                    "Air conditioner",
-                    "Balcony",
-                    "Bed",
-                    "Covered car parking",
-                    "Refrigerator",
-                    "Washing machine",
-                    "Water heater",
-                    "Wardrobe",
-                    "Sofa set",
-                    "Dining table",
-                    "Microwave oven",
-                    "TV unit",
-                    "High-speed WiFi",
-                    "24-hour security"
-            );
-
-            List<String> commonFacilities = new ArrayList<>(Arrays.asList(
-                    "Gym",
-                    "Balcony",
-                    "Covered car parking",
-                    "TV unit",
-                    "High-speed WiFi",
-                    "24-hour security"
-            ));
-
-            List<String> imageUrls = new ArrayList<>(Arrays.asList(
-                    "assets/images/commercial-hero.webp",
-                    "assets/images/kl.webp",
-                    "assets/images/kl-rent-bg.webp",
-                    "assets/images/commercial-hero.webp",
-                    "assets/images/buy-hero.webp",
-                    "assets/images/kl-rent-bg.webp"
-            ));
-
             PropertyDTO propertyDTO = new PropertyDTO();
             propertyDTO.setId(1l);
             propertyDTO.setName("Axis Crown @ Axis Pandan");
@@ -99,32 +99,6 @@ public class PropertyService
             properties.add(propertyDTO3);
 
         }else if(type.equalsIgnoreCase("buy")){
-            List<String> amenities = Arrays.asList(
-                    "Air conditioner",
-                    "Balcony",
-                    "Bed",
-                    "Covered car parking",
-                    "Refrigerator",
-                    "Washing machine",
-                    "Water heater",
-                    "Wardrobe",
-                    "Sofa set",
-                    "Dining table",
-                    "Microwave oven",
-                    "TV unit",
-                    "High-speed WiFi",
-                    "24-hour security"
-            );
-
-            List<String> commonFacilities = new ArrayList<>(Arrays.asList(
-                    "Gym",
-                    "Balcony",
-                    "Covered car parking",
-                    "TV unit",
-                    "High-speed WiFi",
-                    "24-hour security"
-            ));
-
             PropertyDTO propertyDTO = new PropertyDTO();
             propertyDTO.setId(1l);
             propertyDTO.setName("Axis Crown @ Axis Pandan");
@@ -138,36 +112,27 @@ public class PropertyService
             propertyDTO.setAmenities(amenities);
             propertyDTO.setCommonFacilities(commonFacilities);
             propertyDTO.setLocation("https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d15930.970407806825!2d101.68405868809338!3d3.1390036499999967!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x31cc37d111111111%3A0x7a0b1d362bc3a0!2sKuala%20Lumpur%2C%20Federal%20Territory%20of%20Kuala%20Lumpur%2C%20Malaysia!5e0!3m2!1sen!2smy!4v1661430783654!5m2!1sen!2smy");
+            propertyDTO.setImageUrls(imageUrls);
+
+            PropertyDTO propertyDTO2 = new PropertyDTO();
+            propertyDTO2.setId(2l);
+            propertyDTO2.setName("Axis Crown @ Axis Pandan");
+            propertyDTO2.setAddress("(Pandan Indah, Ampang)");
+            propertyDTO2.setPrice(600000l);
+            propertyDTO2.setBedrooms(3);
+            propertyDTO2.setBathrooms(3);
+            propertyDTO2.setCarParks(2);
+            propertyDTO2.setFurnishing("Fully Furnished");
+            propertyDTO2.setSizeSqft(950);
+            propertyDTO2.setAmenities(amenities);
+            propertyDTO2.setCommonFacilities(commonFacilities);
+            propertyDTO2.setLocation("https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d15930.970407806825!2d101.68405868809338!3d3.1390036499999967!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x31cc37d111111111%3A0x7a0b1d362bc3a0!2sKuala%20Lumpur%2C%20Federal%20Territory%20of%20Kuala%20Lumpur%2C%20Malaysia!5e0!3m2!1sen!2smy!4v1661430783654!5m2!1sen!2smy");
+            propertyDTO2.setImageUrls(imageUrls);
 
             properties.add(propertyDTO);
+            properties.add(propertyDTO2);
         }
         else if(type.equalsIgnoreCase("commercial")){
-            List<String> amenities = Arrays.asList(
-                    "Air conditioner",
-                    "Balcony",
-                    "Bed",
-                    "Covered car parking",
-                    "Refrigerator",
-                    "Washing machine",
-                    "Water heater",
-                    "Wardrobe",
-                    "Sofa set",
-                    "Dining table",
-                    "Microwave oven",
-                    "TV unit",
-                    "High-speed WiFi",
-                    "24-hour security"
-            );
-
-            List<String> commonFacilities = new ArrayList<>(Arrays.asList(
-                    "Gym",
-                    "Balcony",
-                    "Covered car parking",
-                    "TV unit",
-                    "High-speed WiFi",
-                    "24-hour security"
-            ));
-
             PropertyDTO propertyDTO = new PropertyDTO();
             propertyDTO.setId(1l);
             propertyDTO.setName("Axis Crown @ Axis Pandan");
@@ -181,36 +146,27 @@ public class PropertyService
             propertyDTO.setAmenities(amenities);
             propertyDTO.setCommonFacilities(commonFacilities);
             propertyDTO.setLocation("https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d15930.970407806825!2d101.68405868809338!3d3.1390036499999967!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x31cc37d111111111%3A0x7a0b1d362bc3a0!2sKuala%20Lumpur%2C%20Federal%20Territory%20of%20Kuala%20Lumpur%2C%20Malaysia!5e0!3m2!1sen!2smy!4v1661430783654!5m2!1sen!2smy");
+            propertyDTO.setImageUrls(imageUrls);
+
+            PropertyDTO propertyDTO2 = new PropertyDTO();
+            propertyDTO2.setId(2l);
+            propertyDTO2.setName("Axis Crown @ Axis Pandan");
+            propertyDTO2.setAddress("(Pandan Indah, Ampang)");
+            propertyDTO2.setPrice(900000l);
+            propertyDTO2.setBedrooms(3);
+            propertyDTO2.setBathrooms(2);
+            propertyDTO2.setCarParks(3);
+            propertyDTO2.setFurnishing("Fully Furnished");
+            propertyDTO2.setSizeSqft(750);
+            propertyDTO2.setAmenities(amenities);
+            propertyDTO2.setCommonFacilities(commonFacilities);
+            propertyDTO2.setLocation("https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d15930.970407806825!2d101.68405868809338!3d3.1390036499999967!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x31cc37d111111111%3A0x7a0b1d362bc3a0!2sKuala%20Lumpur%2C%20Federal%20Territory%20of%20Kuala%20Lumpur%2C%20Malaysia!5e0!3m2!1sen!2smy!4v1661430783654!5m2!1sen!2smy");
+            propertyDTO2.setImageUrls(imageUrls);
 
             properties.add(propertyDTO);
+            properties.add(propertyDTO2);
         }
         else if(type.equalsIgnoreCase("mm2h")){
-            List<String> amenities = Arrays.asList(
-                    "Air conditioner",
-                    "Balcony",
-                    "Bed",
-                    "Covered car parking",
-                    "Refrigerator",
-                    "Washing machine",
-                    "Water heater",
-                    "Wardrobe",
-                    "Sofa set",
-                    "Dining table",
-                    "Microwave oven",
-                    "TV unit",
-                    "High-speed WiFi",
-                    "24-hour security"
-            );
-
-            List<String> commonFacilities = new ArrayList<>(Arrays.asList(
-                    "Gym",
-                    "Balcony",
-                    "Covered car parking",
-                    "TV unit",
-                    "High-speed WiFi",
-                    "24-hour security"
-            ));
-
             PropertyDTO propertyDTO = new PropertyDTO();
             propertyDTO.setId(1l);
             propertyDTO.setName("Axis Crown @ Axis Pandan");
@@ -224,36 +180,27 @@ public class PropertyService
             propertyDTO.setAmenities(amenities);
             propertyDTO.setCommonFacilities(commonFacilities);
             propertyDTO.setLocation("https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d15930.970407806825!2d101.68405868809338!3d3.1390036499999967!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x31cc37d111111111%3A0x7a0b1d362bc3a0!2sKuala%20Lumpur%2C%20Federal%20Territory%20of%20Kuala%20Lumpur%2C%20Malaysia!5e0!3m2!1sen!2smy!4v1661430783654!5m2!1sen!2smy");
+            propertyDTO.setImageUrls(imageUrls);
+
+            PropertyDTO propertyDTO2 = new PropertyDTO();
+            propertyDTO2.setId(2l);
+            propertyDTO2.setName("Axis Crown @ Axis Pandan");
+            propertyDTO2.setAddress("(Pandan Indah, Ampang)");
+            propertyDTO2.setPrice(950000l);
+            propertyDTO2.setBedrooms(3);
+            propertyDTO2.setBathrooms(2);
+            propertyDTO2.setCarParks(4);
+            propertyDTO2.setFurnishing("Fully Furnished");
+            propertyDTO2.setSizeSqft(750);
+            propertyDTO2.setAmenities(amenities);
+            propertyDTO2.setCommonFacilities(commonFacilities);
+            propertyDTO2.setLocation("https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d15930.970407806825!2d101.68405868809338!3d3.1390036499999967!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x31cc37d111111111%3A0x7a0b1d362bc3a0!2sKuala%20Lumpur%2C%20Federal%20Territory%20of%20Kuala%20Lumpur%2C%20Malaysia!5e0!3m2!1sen!2smy!4v1661430783654!5m2!1sen!2smy");
+            propertyDTO2.setImageUrls(imageUrls);
 
             properties.add(propertyDTO);
+            properties.add(propertyDTO2);
         }
-        else if(type.equalsIgnoreCase("newProject")){
-            List<String> amenities = Arrays.asList(
-                    "Air conditioner",
-                    "Balcony",
-                    "Bed",
-                    "Covered car parking",
-                    "Refrigerator",
-                    "Washing machine",
-                    "Water heater",
-                    "Wardrobe",
-                    "Sofa set",
-                    "Dining table",
-                    "Microwave oven",
-                    "TV unit",
-                    "High-speed WiFi",
-                    "24-hour security"
-            );
-
-            List<String> commonFacilities = new ArrayList<>(Arrays.asList(
-                    "Gym",
-                    "Balcony",
-                    "Covered car parking",
-                    "TV unit",
-                    "High-speed WiFi",
-                    "24-hour security"
-            ));
-
+        else if(type.equalsIgnoreCase("newProjects")){
             PropertyDTO propertyDTO = new PropertyDTO();
             propertyDTO.setId(1l);
             propertyDTO.setName("Axis Crown @ Axis Pandan");
@@ -267,8 +214,25 @@ public class PropertyService
             propertyDTO.setAmenities(amenities);
             propertyDTO.setCommonFacilities(commonFacilities);
             propertyDTO.setLocation("https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d15930.970407806825!2d101.68405868809338!3d3.1390036499999967!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x31cc37d111111111%3A0x7a0b1d362bc3a0!2sKuala%20Lumpur%2C%20Federal%20Territory%20of%20Kuala%20Lumpur%2C%20Malaysia!5e0!3m2!1sen!2smy!4v1661430783654!5m2!1sen!2smy");
+            propertyDTO.setImageUrls(imageUrls);
+
+            PropertyDTO propertyDTO2 = new PropertyDTO();
+            propertyDTO2.setId(2l);
+            propertyDTO2.setName("Axis Crown @ Axis Pandan");
+            propertyDTO2.setAddress("(Pandan Indah, Ampang)");
+            propertyDTO2.setPrice(800000l);
+            propertyDTO2.setBedrooms(4);
+            propertyDTO2.setBathrooms(2);
+            propertyDTO2.setCarParks(3);
+            propertyDTO2.setFurnishing("Fully Furnished");
+            propertyDTO2.setSizeSqft(750);
+            propertyDTO2.setAmenities(amenities);
+            propertyDTO2.setCommonFacilities(commonFacilities);
+            propertyDTO2.setLocation("https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d15930.970407806825!2d101.68405868809338!3d3.1390036499999967!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x31cc37d111111111%3A0x7a0b1d362bc3a0!2sKuala%20Lumpur%2C%20Federal%20Territory%20of%20Kuala%20Lumpur%2C%20Malaysia!5e0!3m2!1sen!2smy!4v1661430783654!5m2!1sen!2smy");
+            propertyDTO2.setImageUrls(imageUrls);
 
             properties.add(propertyDTO);
+            properties.add(propertyDTO2);
         }
 
         return properties;
