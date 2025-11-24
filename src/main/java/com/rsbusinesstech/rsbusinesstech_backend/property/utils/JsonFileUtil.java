c1c091bb5b7b25f90d38923a7be787354651b64c
RS Business Tech <rsbusinesstech@gmail.com>
2025-11-24 22:17:36 +0530

    changing file path

--- src/main/java/com/rsbusinesstech/rsbusinesstech_backend/property/utils/JsonFileUtil.java
+++ src/main/java/com/rsbusinesstech/rsbusinesstech_backend/property/utils/JsonFileUtil.java
@@ -3,6 +3,7 @@ package com.rsbusinesstech.rsbusinesstech_backend.property.utils;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.rsbusinesstech.rsbusinesstech_backend.property.dto.PropertyDTO;
+import org.springframework.core.io.ClassPathResource;
 
 import java.io.File;
 import java.io.IOException;
@@ -16,10 +17,13 @@ public class JsonFileUtil {
     //read JSON file.
     public static List<PropertyDTO> readPropertiesByType(String type){
         String filePath = BASE_PATH + type.toLowerCase()+".json";
-        File file = new File(filePath);
+//        File file = new File(filePath); for local only
+        ClassPathResource resource = new ClassPathResource("data/" + type.toLowerCase() + ".json");
+
         TypeReference<List<PropertyDTO>> typeReference = new TypeReference <List<PropertyDTO>>() {};
         try {
-            return objectMapper.readValue(file, typeReference);
+//            return objectMapper.readValue(file, typeReference);
+            return objectMapper.readValue(resource.getInputStream(), typeReference);
         } catch (IOException e) {
             throw new RuntimeException("Error reading JSON: " + filePath, e);
         }
@@ -29,6 +33,7 @@ public class JsonFileUtil {
     public static void writePropertiesByType(String type, List<PropertyDTO> properties){
         String filePath = BASE_PATH + type.toLowerCase()+".json";
         File file = new File(filePath);
+//        ClassPathResource resource = new ClassPathResource("data/" + type.toLowerCase() + ".json");
         try {
             objectMapper.writerWithDefaultPrettyPrinter().writeValue(file, properties);
         } catch (IOException e) {
